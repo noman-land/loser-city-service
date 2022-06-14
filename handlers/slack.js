bbe1276836696c26cea62922d351ca2bd6d8574c
Noman <noman@noman.land>
2022-06-13 20:33:25 -0400

    Log slack events

--- handlers/slack.js
+++ handlers/slack.js
@@ -2,8 +2,14 @@ import { sendSms } from '../utils/smsUtils'
 
 export const handleSlack = async req => {
     const {
-        event: { text, subtype, thread_ts },
+        event: { text, subtype, thread_ts, ...eventRest },
+        ...requestRest
     } = await req.json()
+
+    console.log({
+        ...requestRest,
+        event: { text, subtype, thread_ts, ...eventRest },
+    })
 
     const phoneNumber = await LOSERS.get(thread_ts)
 
