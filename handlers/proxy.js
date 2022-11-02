8aa484f1e8e928a52907110024220770dc06093e
Noman <noman@noman.land>
2022-11-02 03:43:15 -0400

    Remove proxy endpoint

--- handlers/proxy.js
+++
@@ -1,5 +0,0 @@
-export const handleProxy = async request => {
-  const { pathname } = new URL(decodeURIComponent(request.query.i));
-  const [, fileType] = pathname.split('.');
-  return fetch(request.query.i);
-};
